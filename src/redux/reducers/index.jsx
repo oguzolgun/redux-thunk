cd4cd55a7dcf62cdeade878f6a1b044650bcfadd
oguzolgun <oguzolgun44@gmail.com>
2022-04-20 00:52:03 +0300

    created auth firebase

--- src/redux/reducers/index.jsx
+++ src/redux/reducers/index.jsx
@@ -1,11 +1,11 @@
 import { combineReducers } from "redux";
 import appReducer from "./appReducer";
 import newsReducer from "./newsReducer";
-// import {authReducer} from "./authReducer";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
     app: appReducer,
-    // auth:authReducer,
+    auth:authReducer,
     news:newsReducer
 })
 export default rootReducer
